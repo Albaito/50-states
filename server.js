1dc88c153aa2c6b7d90e50558be7bd01f8138f60
Albaito <121689899+Albaito@users.noreply.github.com>
2023-04-14 11:58:04 -0500

    pre-vue app changes

---
+++ server.js
@@ -0,0 +1,21 @@
+let express = require('express')
+let states_api = require('./routes/states')
+
+let app = express()
+
+app.use(express.json()) // enable parsing JSON
+
+app.use('/api', states_api)
+
+app.use(function(req, res, next) {
+    res.status(404).send('Not found')
+})
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack)    // log for developer use
+    res.status(500).send('Server error')
+})
+
+let server = app.listen(process.env.PORT || 3000, function() {
+    console.log('App running on port', server.address().port)
+})
